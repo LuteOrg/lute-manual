a902082a760a1f6f76c5e3305d77f42e6334d916
jzohrab <jzohrab@users.noreply.github.com>
2026-07-26 00:59:56 +0000

    deploy: b5991bb518e3a2f12b205a7e582936b29d33ff9e

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-05f6b7f6.js',
+                path_to_root + 'searchindex-5fc68bcd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
